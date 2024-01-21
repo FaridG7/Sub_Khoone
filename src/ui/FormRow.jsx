02389cfcd98ae1a6c9c7b0063f332aae44bc71be
AbolfazlMoaveni <abolfazlmoaveni.work@gmail.com>
2024-01-21 05:34:52 +0330

    "person form and estateform and formrow"s tailwind is complete

--- src/ui/FormRow.jsx
+++ src/ui/FormRow.jsx
@@ -1,8 +1,8 @@
 function FormRow({ label, error, children }) {
   return (
-    <div className="py-2">
-      {label && <label htmlFor={children.props.id}>{label}</label>}
+    <div className="py-2 text-left space-x-4 flex justify-between">
       {children}
+      {label && <label htmlFor={children.props.id}>{label}</label>}
       {error && <span>{error}</span>}
     </div>
   );
