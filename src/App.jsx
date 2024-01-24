e449408c28cc03728b1a1c2ef649000270a41fad
FaridG <goharifarid7@gmail.com>
2024-01-24 05:16:41 +0330

    changed toast colors

--- src/App.jsx
+++ src/App.jsx
@@ -54,8 +54,8 @@ function App() {
               fontSize: "16px",
               maxWidth: "500px",
               padding: "16px 24px",
-              backgroundColor: "var(--color-grey-0)",
-              color: "var(--color-grey-700)",
+              backgroundColor: "#009933",
+              color: "#ffffff",
             },
           }}
         />
